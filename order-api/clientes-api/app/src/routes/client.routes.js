8d2a815c24a763bc3634b61e06255ac031589ea3
Thomas Toro <thomy0736@gmail.com>
2025-11-30 20:09:57 -0600

    feat: add roles and middlewares

--- order-api/clientes-api/app/src/routes/client.routes.js
+++ order-api/clientes-api/app/src/routes/client.routes.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/client.controller');
 
+const verifyToken = require('../middlewares/auth.middleware');
+const checkRole = require('../middlewares/role.middleware');
+
+
 //Crear
-router.post('/', clientController.createClient);
+router.post('/', verifyToken, checkRole(['EMPLOYEE','MANAGER','ADMIN']), clientController.createClient);
 
 //Obtener todos 
-router.get('/', clientController.getAllClients);
+router.get('/', verifyToken, checkRole(['EMPLOYEE','MANAGER','ADMIN']), clientController.getAllClients);
 
 //Obtener uno
-router.get('/:id', clientController.getClient);
+router.get('/:id', verifyToken, checkRole(['EMPLOYEE','MANAGER','ADMIN']), clientController.getClient);
 
 //Editar
-router.patch('/:id', clientController.editClient);
+router.patch('/:id', verifyToken, checkRole(['EMPLOYEE','MANAGER','ADMIN']),clientController.editClient);
 
 //Eliminar
-router.delete('/:id', clientController.deleteClient);
+router.delete('/:id', verifyToken, checkRole(['ADMIN']), clientController.deleteClient);
 
 module.exports = router;
